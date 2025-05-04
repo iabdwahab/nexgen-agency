670cb34407fe6ba327e886cdb4d946a127b88854
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-04 05:55:07 +0300

    Update MaxWidth of Container.

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -1,12 +1,10 @@
-import logoLarge from "/logo/logo-large.svg";
 import logoMedium from "/logo/logo-medium.svg";
 import logoSmall from "/logo/logo-small.svg";
 
 function Logo() {
   return (
     <picture>
-      <source media="(min-width:1024px)" srcSet={logoLarge} />
-      <source media="(min-width:768px)" srcSet={logoMedium} />
+      <source media="(min-width:1024px)" srcSet={logoMedium} />
       <img src={logoSmall} alt="NexGen Logo" />
     </picture>
   );
