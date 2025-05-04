332c58596c081dac77a3497387563ccec8e6837c
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-04 04:30:32 +0300

    Adding Simple Error 404 Page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { HashRouter, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
+import Error from "./pages/Error";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Home />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </HashRouter>
---
+++ src/pages/Error.tsx
@@ -0,0 +1,10 @@
+function Error() {
+  return (
+    <main>
+      <div className="container py-8">
+        <h1 className="font-roboto-mono text-center text-2xl font-medium">Sorry, this page does not exist. Please check the URL and try again.</h1>
+      </div>
+    </main>
+  );
+}
+export default Error;
