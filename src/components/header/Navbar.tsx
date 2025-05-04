d987367c6f77fe7ffce84d1edfdb97279475ce13
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-04 04:16:47 +0300

    Change Last Item Color on Navbar.

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -3,7 +3,7 @@ import { navbarLinksElements } from "../../data/Navbar";
 function Navbar() {
   return (
     <nav className="font-roboto-mono text-grey-50 bg-dark-10 absolute top-[calc(100%+15px)] left-0 w-full rounded-[10px] px-3 py-6 font-medium uppercase lg:relative lg:w-fit lg:px-0 lg:py-0">
-      <ul className="flex flex-col gap-2 lg:flex-row">{navbarLinksElements}</ul>
+      <ul className="[&_li:last-child_a]:bg-orange-60 [&_li:last-child_a]:text-dark-06 flex flex-col gap-2 lg:flex-row">{navbarLinksElements}</ul>
     </nav>
   );
 }
