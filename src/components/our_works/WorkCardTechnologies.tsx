a6587a0dbda705eb70255af5409450ae0d8432b3
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2025-05-05 09:05:54 +0300

    (our-works): Fixing Design Bug.

--- src/components/our_works/WorkCardTechnologies.tsx
+++ src/components/our_works/WorkCardTechnologies.tsx
@@ -6,7 +6,7 @@ function WorkCardTechnologies({ data }: { data: string[] }) {
   ));
 
   return (
-    <div className="bg-dark-10 rounded-[10px] p-6">
+    <div className="bg-dark-10 h-full rounded-[10px] p-6">
       <h3 className="text-base font-medium text-white uppercase">technologies used</h3>
       <div className="mt-4 flex flex-wrap gap-2">{technologiesElements}</div>
     </div>
